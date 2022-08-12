6942e8ed1edd6bc194e94ce10c03da60853cfcde
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-08-12 21:48:23 +0700

    Added Tailwind CSS and its dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles/tailwind.css";
